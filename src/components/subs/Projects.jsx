fd77d4ae42a0033237a88d5f67c356a8e90130c2
moohka <moohka@hotmail.com>
2022-08-26 21:01:21 -0500

    Project style

--- src/components/subs/Projects.jsx
+++ src/components/subs/Projects.jsx
@@ -1,63 +1,10 @@
 import { useRef, useEffect } from "react";
-import Keep from "../../assets/screenshots/Screenshot_of_Google_Keep_Clone.png";
-import Olive from "../../assets/screenshots/Screenshot_of_Olive_Hair.png";
+// import Keep from "../../assets/screenshots/Screenshot_of_Google_Keep_Clone.png";
+// import Olive from "../../assets/screenshots/Screenshot_of_Olive_Hair.png";
 
 const Projects = () => {
-  //project hover effect
-  const firstRef = useRef();
-  const secondRef = useRef();
-  const thirdRef = useRef();
-  // const fourthRef = useRef();
-
-  // function showDetail(theRef) {
-  //   theRef.current.classList.toggle("hovered");
-  //   console.log(theRef.current.classList);
-  //   console.log("enter");
-  // }
-
-  // function hideDetail(theRef) {
-  //   theRef.current.classList.toggle("hovered");
-  //   console.log("leave");
-  // }
-
   //projects
-  const myProjects = [
-    {
-      id: 1,
-      ref: firstRef,
-      img: null,
-      title: "Poultry Management Software",
-      detail:
-        "I made this software as a final project of Java course when I was enrolled in Software Development program.",
-      builtWith: ["Java", "mySQL"],
-      view: { live: null, repo: "https:www.github.com" },
-    },
-    {
-      id: 2,
-      ref: secondRef,
-      img: Olive,
-      title: "Olive Hair Salon",
-      detail: "Calgary local hairsalon.",
-      builtWith: ["React", "Sass"],
-      view: {
-        live: "https://olivehair2.netlify.app",
-        repo: "https:www.github.com",
-      },
-    },
-    {
-      id: 3,
-      ref: thirdRef,
-      img: Keep,
-      title: "Google Keep Clone",
-      detail:
-        "Clone website of Google Keep. This website demonstrates my knowledge on Firebase and data manipulation",
-      builtWith: ["React", "Firebase", "Sass"],
-      view: {
-        live: "https://olivehair2.netlify.app",
-        repo: "https:www.github.com",
-      },
-    },
-  ];
+  let myProjects;
 
   return (
     <div className="sub" id="projects">
