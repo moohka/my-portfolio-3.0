3ab0ecb1a113f24c15cbb47d2801ef3a5b7f6375
moohka <moohka@hotmail.com>
2022-11-27 19:19:56 -0600

    re-organized

---
+++ src/components/App/App.jsx
@@ -0,0 +1,23 @@
+import Header from "../layouts/Header/Header";
+import Footer from "../layouts/Footer/Footer";
+import Home from "../mains/Home/Home";
+import About from "../mains/About/About";
+import Contact from "../mains/Contact/Contact";
+import { NavProvider } from "../../contexts/NavContext";
+
+const App = () => {
+  return (
+    <div className="app">
+      <NavProvider>
+        <Header />
+        <Home />
+        <About />
+        <Contact />
+      </NavProvider>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./apps/App/App";
+import App from "./components/App/App";
 import "./styles/style.css";
 
 import AOS from "aos";
@@ -9,7 +9,7 @@ AOS.init({ once: true });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
