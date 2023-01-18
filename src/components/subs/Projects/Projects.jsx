c7142ce72d08a866b81c289536ccab7bdf6a7a40
moohka <moohka@hotmail.com>
2023-01-17 23:34:41 -0600

    The order of the projects and About padding update

--- src/components/subs/Projects/Projects.jsx
+++ src/components/subs/Projects/Projects.jsx
@@ -4,26 +4,26 @@ import MYPROJECTS from "../../../assets/database/myProjects.json";
 
 const ProjectContainer = () => {
   //attaching images to the project database
-  MYPROJECTS[0].images = [
+  MYPROJECTS[0].images = [SCREENSHOTS.Salon];
+  MYPROJECTS[1].images = [SCREENSHOTS.Bookstore];
+  MYPROJECTS[2].images = [SCREENSHOTS.Keep];
+  MYPROJECTS[3].images = [
     SCREENSHOTS.Poultry,
     SCREENSHOTS.Poultry1,
     SCREENSHOTS.Poultry2,
     SCREENSHOTS.Poultry3,
     SCREENSHOTS.Poultry4,
   ];
-  MYPROJECTS[1].images = [SCREENSHOTS.Bookstore];
-  MYPROJECTS[2].images = [SCREENSHOTS.Keep];
-  MYPROJECTS[3].images = [SCREENSHOTS.Salon];
 
   return (
     <div className="projects">
       <h2 className="project-sub-title">My Projects</h2>
 
       <div className="project-container">
-        <ProjectCard project={MYPROJECTS[0]} formatId="multi-img" />
+        <ProjectCard project={MYPROJECTS[0]} formatId="left-img" />
         <ProjectCard project={MYPROJECTS[1]} formatId="right-img" />
         <ProjectCard project={MYPROJECTS[2]} formatId="left-img" />
-        <ProjectCard project={MYPROJECTS[3]} formatId="right-img" />
+        <ProjectCard project={MYPROJECTS[3]} formatId="multi-img" />
       </div>
     </div>
   );
