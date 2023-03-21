31a9cd51e9981c39b758c05286f81aa8ec29479b
moohka <moohka@hotmail.com>
2023-03-21 12:46:31 -0500

    Hero page heroRef added back

--- src/components/sections/1Hero/Hero.jsx
+++ src/components/sections/1Hero/Hero.jsx
@@ -1,6 +1,10 @@
+import { NavContextValue } from "../../../contexts/NavContext";
+
 const Hero = () => {
+  const { heroRef } = NavContextValue();
+
   return (
-    <section className="main-section" id="hero-section">
+    <section className="main-section" id="hero-section" ref={heroRef}>
       <canvas className="hero-art"></canvas>
     </section>
   );
