4f54aff5c4955e4d24ba244cc470601c5e18c06b
moohka <moohka@hotmail.com>
2023-03-26 00:40:05 -0500

    Project section pagination

--- src/components/sections/3Project/Project.jsx
+++ src/components/sections/3Project/Project.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import ProjectCard from "../../cards/ProjectCard/ProjectCard";
 import MYPROJECTS from "../../../assets/database/myProjects.json";
 import { NavContextValue } from "../../../contexts/NavContext";
+import { ReactComponent as Arrow } from "../../../assets/visuals/arrow.svg";
 
 const ProjectContainer = () => {
   const { projectRef } = NavContextValue();
+  const [page, setPage] = useState("first-page");
 
   return (
     <section className="main-section" id="project-section" ref={projectRef}>
@@ -11,9 +14,28 @@ const ProjectContainer = () => {
         <h2 className="project-heading">Projects</h2>
 
         <div className="project-card-container">
-          {MYPROJECTS.map((project, index) => (
-            <ProjectCard key={index} project={project} />
-          ))}
+          <div className={`pagination-container ` + page}>
+            {MYPROJECTS.map((project, index) => {
+              return <ProjectCard key={index} project={project} />;
+            })}
+          </div>
+        </div>
+
+        <div className="button-container">
+          <button
+            className={page}
+            id="up"
+            onClick={() => setPage("first-page")}
+          >
+            <Arrow />
+          </button>
+          <button
+            className={page}
+            id="down"
+            onClick={() => setPage("second-page")}
+          >
+            <Arrow />
+          </button>
         </div>
       </div>
     </section>
