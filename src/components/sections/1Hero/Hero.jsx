8475caaab5e83ced99432452b7dddbfb7acfdfa9
moohka <moohka@hotmail.com>
2023-03-31 20:27:07 -0500

    The Sphere on hero section complete

--- src/components/sections/1Hero/Hero.jsx
+++ src/components/sections/1Hero/Hero.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useRef, useEffect } from "react";
 import * as THREE from "three";
 import { NavContextValue } from "../../../contexts/NavContext";
@@ -16,9 +17,10 @@ const Hero = () => {
     const geometry = new THREE.SphereGeometry(4.5, 100, 100);
     const material = new THREE.MeshStandardMaterial({
       color: 0xfdfdfd,
-      roughness: 0.5,
+      roughness: 0.4,
     });
     const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.y -= 1; //zoom
 
     //camera
     const camera = new THREE.PerspectiveCamera(
@@ -28,15 +30,19 @@ const Hero = () => {
       0.1,
       1000
     );
-    camera.position.z = 10;
+    camera.position.z = 1;
     camera.aspect =
       document.documentElement.clientWidth /
       document.documentElement.clientHeight;
     camera.updateProjectionMatrix();
 
     //lighting
-    const light = new THREE.PointLight(0xfdfdfd, 1, 150);
-    light.position.set(0, 10, -5);
+    const light = new THREE.PointLight(0xfdfdfd, 1, 90);
+    light.position.set(0, 25, -30); //position
+
+    // create an ambient light with a color and intensity
+    const ambientLight = new THREE.AmbientLight(0x020202, 1);
+    scene.add(ambientLight);
 
     //renderer
     const renderer = new THREE.WebGLRenderer();
@@ -55,10 +61,11 @@ const Hero = () => {
     //render the scene
     renderer.render(scene, camera);
 
-    // animation
+    // light reveal animation
     const animate = (e) => {
-      if (light.position.z < 14) {
-        light.position.z += 0.03; //speed
+      //position
+      if (light.position.z < 10) {
+        light.position.z += 0.3; //speed
         scene.add(light);
         renderer.render(scene, camera);
         requestAnimationFrame(animate);
@@ -66,27 +73,72 @@ const Hero = () => {
     };
     animate();
 
-    const handleWindowResize = () => {
+    //1. resizing function
+    function handleWindowResize() {
       let width = document.documentElement.clientWidth;
       let height = document.documentElement.clientHeight;
 
-      camera.position.z = 10;
+      if (window.innerWidth < 440) {
+        camera.position.z = 15; //zoom
+      } else if (window.innerWidth < 680) {
+        camera.position.z = 13; //zoom
+      } else {
+        camera.position.z = 9; //zoom
+      }
       camera.aspect = width / height;
       renderer.setSize(width, height);
 
       camera.updateProjectionMatrix();
       renderer.render(scene, camera);
-    };
-
-    handleWindowResize();
-    window.addEventListener("resize", handleWindowResize);
-    if (light.position.z < 14) {
     }
 
+    //2. mousemove effect function
+    function sphereMouseEffect(e) {
+      //position
+      //-5 to 5
+      let mousePositionPercentage = (e.clientX * 2) / window.innerWidth - 1;
+      let lightPosition = 5 * mousePositionPercentage;
+
+      //position
+      //-10 to 5
+      let mousePositionY = e.clientY / window.innerHeight;
+      let lightPositionZ = -10 + (8 - -10) * mousePositionY + 10;
+
+      let startTime = new Date().getTime(); // Get the current time in milliseconds
+      let duration = 3000; // 3 seconds
+
+      // Define a function to update the light's position based on elapsed time
+      function updatePosition() {
+        let elapsedTime = new Date().getTime() - startTime; // Calculate elapsed time
+        let t = Math.min(1, elapsedTime / duration); // Calculate progress as a value between 0 and 1
+
+        // Interpolate between the current position and the target position using the progress value
+        light.position.x =
+          light.position.x + (lightPosition - light.position.x) * t;
+        light.position.z =
+          light.position.z + (lightPositionZ - light.position.z) * t;
+
+        renderer.render(scene, camera);
+
+        if (t < 1) {
+          // If the animation is not complete, request the next frame of the animation
+          requestAnimationFrame(updatePosition);
+        }
+      }
+      // Start the animation
+      requestAnimationFrame(updatePosition);
+    }
+
+    //activate
+    handleWindowResize();
+    window.addEventListener("resize", handleWindowResize);
+    setTimeout(() => {
+      canvasRef.current.addEventListener("mousemove", sphereMouseEffect);
+    }, 2000);
+
     return () => {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
       canvasRef.current?.removeChild(renderer.domElement);
-      window.removeEventListener("resize", handleWindowResize);
+      canvasRef.current.removeEventListener("mousemove", sphereMouseEffect);
     };
   }, []);
 
