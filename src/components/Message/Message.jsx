c55d103631af5dcaad8ae0fe567374682d7a5c10
moohka <moohka@hotmail.com>
2023-07-26 05:11:29 -0500

    Complete

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,11 +1,7 @@
 const Message = () => {
   return (
     <div className="message">
-      <p>
-        expedita minima maxime eligendi aut reprehenderit, veritatis recusandae
-        porro iure harum consequuntur cupiditate reiciendis, quibusdam labore.
-        Expedita ducimus animi possimus alias.
-      </p>
+      <div className="text-container"></div>
     </div>
   );
 };
